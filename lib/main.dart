87baac8b9c69f0cce4ce22d36a97ae24a730ffd7
KoJaem <woalswhwh@naver.com>
2023-07-30 23:33:42 +0900

    feat : flutter style 기초 공부

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,35 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Heelo fltter!"),
-        ),
-        body: Center(
-          child: Text("Hello wolrd!"),
+        backgroundColor: Color(0xFF181818), // 색상 #181818 을 주는법
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 40),
+          child: Column(children: [
+            SizedBox(
+              height: 80,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text("Hey, Selena",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                        )),
+                    Text(
+                      'Welcome back',
+                      style: TextStyle(
+                          color: Colors.white.withOpacity(0.8), fontSize: 18),
+                    ),
+                  ],
+                )
+              ],
+            )
+          ]),
         ),
       ),
     );
