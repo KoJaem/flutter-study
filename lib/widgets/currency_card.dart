e11f21aa8ba33a646f3fd56cea666094852da0a8
KoJaem <woalswhwh@naver.com>
2023-08-06 22:01:44 +0900

    feat : 공용컴포넌트 사용 공부

---
+++ lib/widgets/currency_card.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+
+class CurrencyCard extends StatelessWidget {
+  final String name, code, amount;
+  final IconData icon;
+  final bool isInverted;
+
+  const CurrencyCard({
+    super.key,
+    required this.name,
+    required this.code,
+    required this.amount,
+    required this.icon,
+    this.isInverted = false,
+  });
+
+  // = Inverted ? Colors.white : Colors.black;
+
+  @override
+  Widget build(BuildContext context) {
+    final Color bgColor = isInverted ? Colors.white : const Color(0xFF1F2123);
+    final Color textColor = isInverted ? const Color(0xFF1F2123) : Colors.white;
+
+    return Container(
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+        color: bgColor,
+        borderRadius: BorderRadius.circular(25),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(30),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  name,
+                  style: TextStyle(
+                    color: textColor,
+                    fontSize: 32,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      amount,
+                      style: TextStyle(
+                        color: textColor,
+                        fontSize: 20,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    Text(code,
+                        style: TextStyle(
+                          color: textColor.withOpacity(0.8),
+                          fontSize: 20,
+                        )),
+                  ],
+                ),
+              ],
+            ),
+            Transform.scale(
+              scale: 2.2,
+              child: Transform.translate(
+                offset: const Offset(-5, 12),
+                child: Icon(
+                  icon,
+                  color: textColor,
+                  size: 88,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+/////////////// isInverted 가 아닌, 컬러값 받아서 하는 컴포넌트
+
+// import 'package:flutter/material.dart';
+
+// class CurrencyCard extends StatelessWidget {
+//   final String name, code, amount;
+//   final Color textColor, bgColor, iconColor;
+//   final IconData icon;
+
+//   const CurrencyCard({
+//     super.key,
+//     required this.name,
+//     required this.code,
+//     required this.amount,
+//     required this.bgColor,
+//     required this.icon,
+//     required this.iconColor,
+//     required this.textColor,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       clipBehavior: Clip.hardEdge,
+//       decoration: BoxDecoration(
+//         color: bgColor,
+//         borderRadius: BorderRadius.circular(25),
+//       ),
+//       child: Padding(
+//         padding: const EdgeInsets.all(30),
+//         child: Row(
+//           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//           children: [
+//             Column(
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               children: [
+//                 Text(
+//                   name,
+//                   style: TextStyle(
+//                     color: textColor,
+//                     fontSize: 32,
+//                     fontWeight: FontWeight.w600,
+//                   ),
+//                 ),
+//                 const SizedBox(
+//                   height: 10,
+//                 ),
+//                 Row(
+//                   children: [
+//                     Text(
+//                       amount,
+//                       style: TextStyle(
+//                         color: textColor,
+//                         fontSize: 20,
+//                       ),
+//                     ),
+//                     const SizedBox(
+//                       width: 5,
+//                     ),
+//                     Text(code,
+//                         style: TextStyle(
+//                           color: textColor.withOpacity(0.8),
+//                           fontSize: 20,
+//                         )),
+//                   ],
+//                 ),
+//               ],
+//             ),
+//             Transform.scale(
+//               scale: 2.2,
+//               child: Transform.translate(
+//                 offset: const Offset(-5, 12),
+//                 child: Icon(
+//                   icon,
+//                   color: iconColor,
+//                   size: 88,
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
