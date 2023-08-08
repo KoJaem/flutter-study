4370daef5d59ae68f4961d3cbd43d3930fd86982
KoJaem <woalswhwh@naver.com>
2023-08-08 13:27:07 +0900

    feat : buildContext 공부

--- lib/main.dart
+++ lib/main.dart
@@ -12,41 +12,40 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  List<int> numbers = [];
-
-  void onClicked() {
-    setState(() {
-      numbers.add(numbers.length);
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: const Color(0xFFF4EDDB),
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
+      home: const Scaffold(
+        backgroundColor: Color(0xFFF4EDDB),
         body: Center(
           child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            const Text(
-              'Click Count',
-              style: TextStyle(fontSize: 30),
-            ),
-            for (var n in numbers)
-              Text(
-                '$n',
-                style: const TextStyle(
-                  color: Colors.blueAccent,
-                ),
-              ),
-            IconButton(
-              onPressed: onClicked,
-              iconSize: 40,
-              icon: const Icon(
-                Icons.add_box_rounded,
-              ),
-            )
+            MyLargeTitle(),
           ]),
         ),
+      ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge?.color,
       ),
     );
   }
