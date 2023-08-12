21811efa8f9396416dc9e0fd648984d03c086f1d
KoJaem <woalswhwh@naver.com>
2023-08-13 04:29:26 +0900

    feat : named constructor, Future, jsonDecode 공부

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,14 +1,24 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonsInfo = [];
     final url = Uri.parse('$baseUrl/$today');
     try {
       final response = await http.get(url);
-      print(response.body);
+      final List<dynamic> webtoons = jsonDecode(response.body);
+
+      for (var webtoon in webtoons) {
+        final webtoonInfo = WebtoonModel.fromJson(webtoon);
+        webtoonsInfo.add(webtoonInfo);
+      }
+      return webtoonsInfo;
     } catch (error) {
       throw Error();
     }
