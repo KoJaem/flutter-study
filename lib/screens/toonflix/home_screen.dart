c9d66f49f4408cc14970fdfe31608fd79146a69b
KoJaem <woalswhwh@naver.com>
2023-08-17 13:47:18 +0900

    feat : ListView, ListView Builder, ListView separated 공부

--- lib/screens/toonflix/home_screen.dart
+++ lib/screens/toonflix/home_screen.dart
@@ -5,7 +5,7 @@ import 'package:toonflix/services/api_service.dart';
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
 
-  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -22,9 +22,24 @@ class HomeScreen extends StatelessWidget {
       body: FutureBuilder(
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return const Text("There is data!");
+            return ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                // 사용자가 보고있는 화면에 대한 item 만 build 함
+                print(index);
+                var webtoon = snapshot.data![index];
+                return Text(webtoon.title);
+              },
+              separatorBuilder: (context, index) => const SizedBox(
+                // item 들을 구분하기 위해 item 사이에 들어가는 아이템
+                width: 20,
+              ),
+            );
           }
-          return const Text("Loading...");
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
         },
         future: webtoons,
       ),
