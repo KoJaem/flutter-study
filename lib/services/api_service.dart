f35080f0c26b4cedabbea2de1fc6beaf6f89aabb
KoJaem <woalswhwh@naver.com>
2023-08-20 18:57:00 +0900

    feat : API Model 관련 공부

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_detail_model.dart';
+import 'package:toonflix/models/webtoon_episode_model.dart';
 import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
@@ -8,7 +10,7 @@ class ApiService {
   static const String today = "today";
 
   static Future<List<WebtoonModel>> getTodaysToons() async {
-    List<WebtoonModel> webtoonsInfo = [];
+    List<WebtoonModel> webtoonsInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     try {
       final response = await http.get(url);
@@ -16,17 +18,39 @@ class ApiService {
 
       for (var webtoon in webtoons) {
         final webtoonInfo = WebtoonModel.fromJson(webtoon);
-        webtoonsInfo.add(webtoonInfo);
+        webtoonsInstances.add(webtoonInfo);
       }
-      return webtoonsInfo;
+      return webtoonsInstances;
     } catch (error) {
       throw Error();
     }
+  }
 
-    // if (response.statusCode == 200) {
-    //   print(response);
-    //   return;
-    // }
-    // throw Error();
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse("$baseUrl/$id");
+    try {
+      final response = await http.get(url);
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    } catch (error) {
+      throw Error();
+    }
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodeById(
+      String id) async {
+    List<WebtoonEpisodeModel> episodesInstances = [];
+    final url = Uri.parse("$baseUrl/$id/episodes");
+    try {
+      final response = await http.get(url);
+      final episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        final episodeInfo = WebtoonEpisodeModel.formJson(episode);
+        episodesInstances.add(episodeInfo);
+      }
+      return episodesInstances;
+    } catch (error) {
+      throw Error();
+    }
   }
 }
